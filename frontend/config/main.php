86a6579c19ebd3fe8f955de3a3d2b6fc97d49ae8
cc <361623688@qq.com>
2018-03-01 20:56:55 +0800

    yii2advaned

---
+++ frontend/config/main.php
@@ -0,0 +1,51 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'language'=>'zh-CN',
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'urlManager'=>[
+            'enablePrettyUrl'=>true,
+            'showScriptName'=>false,
+            //'suffix'=>'.html',
+        ],
+        //语言包
+        'i18n'=>[
+           'translations'=>[
+               '*'=>[
+                  'class'=>'yii\i18n\PhpMessageSource',
+                   //'basePtah'=>'/messages',
+                   'fileMap'=>[
+                       'common'=>'common.php'
+                   ]
+               ]
+           ],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
